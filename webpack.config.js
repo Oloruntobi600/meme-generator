107d1d3967c54c976efd7651e7f5121b1208fa99
Oloruntobi600 <tobiajayi60@gmail.com>
2024-06-28 11:49:29 +0100

    meme-generator

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  mode: 'development',
+  output: {
+    filename: '[name].pack.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json'],
+    alias: {}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  entry: {
+    index: './index.js'
+  }
+};
