443d979b19baf60052efaa3577fc22ff0cc40d9a
Oloruntobi600 <tobiajayi60@gmail.com>
2024-06-28 15:14:10 +0100

    new update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',  // or 'production'
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].pack.js',
